2364bd5aa2ce7f450e553fe41be50162d335be80
marlon <ogoulamarlon@gmail.com>
2025-04-21 15:42:31 +0100

    Update ALLOWED_HOSTS to include Railway URL

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tzsnn10vju1pxqo(#e1qcoeh4jpwra)j8%s)hj$aokpj-vkvt1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['localhost', '127.0.0.1', 'melodious-trust-production.up.railway.app']
 
 
 # Application definition
